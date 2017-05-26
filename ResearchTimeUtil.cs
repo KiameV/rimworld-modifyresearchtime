afe163c917d395473ca77921219aa18adaca43bc
Travis Offtermatt <travisty2@gmail.com>
2017-05-26 01:13:26 -0600

    Initial commit

---
+++ ResearchTimeUtil.cs
@@ -0,0 +1,79 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace ModifyResearchTime
+{
+    internal static class ResearchTimeUtil
+    {
+        public static Dictionary<string, float> BaseResearchDefs { get; set; }
+
+        static ResearchTimeUtil()
+        {
+            BaseResearchDefs = null;
+        }
+
+        private static void CreateBaseResearchDefs()
+        {
+            if (BaseResearchDefs == null)
+            {
+                BaseResearchDefs = new Dictionary<string, float>();
+                foreach (ResearchProjectDef def in DefDatabase<ResearchProjectDef>.AllDefsListForReading)
+                {
+                    BaseResearchDefs.Add(def.defName, def.baseCost);
+                }
+            }
+        }
+
+        public static void ApplyFactor(float oldFactor, float newFactor)
+        {
+            CreateBaseResearchDefs();
+            Dictionary<string, ResearchProjectDef> defsToModify = new Dictionary<string, ResearchProjectDef>();
+            foreach (ResearchProjectDef def in DefDatabase<ResearchProjectDef>.AllDefs)
+            {
+                defsToModify.Add(def.defName, def);
+            }
+            
+            ResearchManager rm = Find.ResearchManager;
+            Dictionary<string, ResearchProjectDef> researchCompleted = new Dictionary<string, ResearchProjectDef>();
+            Dictionary<ResearchProjectDef, float> progress = 
+                (Dictionary<ResearchProjectDef, float>)rm.GetType().GetField("progress", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(rm);
+            foreach (ResearchProjectDef defToModify in defsToModify.Values)
+            {
+                float baseCost = BaseResearchDefs[defToModify.defName];
+                if (Math.Abs(rm.GetProgress(defToModify) - (baseCost * oldFactor)) < 0.5f)
+                {
+                    researchCompleted.Add(defToModify.defName, null);
+                }
+                else
+                {
+                    float p;
+                    if (progress.TryGetValue(defToModify, out p))
+                    {
+                        if (p > 0)
+                        {
+                            p = (p / oldFactor) * newFactor;
+                            progress[defToModify] = p;
+                        }
+                    }
+                }
+            }
+
+            foreach (ResearchProjectDef defToModify in defsToModify.Values)
+            {
+                float baseCost = BaseResearchDefs[defToModify.defName];
+                defToModify.baseCost = baseCost * newFactor;
+
+                if (researchCompleted.ContainsKey(defToModify.defName))
+                {
+                    rm.InstantFinish(defToModify, false);
+                }
+            }
+
+            defsToModify.Clear();
+            researchCompleted.Clear();
+        }
+    }
+}
---
+++ WorldComp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using RimWorld.Planet;
+using Verse;
+using RimWorld;
+
+namespace ModifyResearchTime
+{
+    class WorldComp : WorldComponent
+    {
+        private static float gameFactor = Settings.SettingsFactor;
+        private static float oldFactor = Settings.SettingsFactor;
+        private static WorldComp Instance;
+        
+        public static bool IsNewGame { get; set; }
+
+        public WorldComp(World world) : base(world)
+        {
+            Instance = this;
+        }
+
+        public override void FinalizeInit()
+        {
+            base.FinalizeInit();
+
+            if (IsNewGame)
+            {
+                ResearchTimeUtil.ApplyFactor(1, Settings.SettingsFactor);
+            }
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look<float>(ref gameFactor, "ModifyResearchTime.Factor", 1f, false);
+            oldFactor = gameFactor;
+            if (Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                IsNewGame = false;
+                ResearchTimeUtil.ApplyFactor(gameFactor, gameFactor);
+            }
+        }
+
+        internal static void UpdateFactor(float settingsFactor)
+        {
+            if (Instance == null)
+            {
+                Log.Error("WorldComp Instance is null.");
+                return;
+            }
+            oldFactor = gameFactor;
+            gameFactor = settingsFactor;
+            if (oldFactor != gameFactor)
+            {
+                ResearchTimeUtil.ApplyFactor(oldFactor, gameFactor);
+            }
+        }
+    }
+}
