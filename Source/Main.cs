08af799adccc3b16f2bdc5dce6f56b35ff67ad88
Travis Offtermatt <travis.offtermatt@gmail.com>
2017-05-26 01:40:43 -0600

    Initial commit

---
+++ Source/Main.cs
@@ -0,0 +1,38 @@
+﻿using Harmony;
+using RimWorld;
+using System.Reflection;
+using Verse;
+
+namespace ModifyResearchTime
+{
+    [StaticConstructorOnStartup]
+    class Main
+    {
+        static Main()
+        {
+            var harmony = HarmonyInstance.Create("com.changedresser.rimworld.mod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            
+            Log.Message("ModifyResearchTime: Adding Harmony Postfix to DefDatabase.ErrorCheckAllDefs");
+        }
+    }
+
+    [HarmonyPatch(typeof(Game), "InitNewGame")]
+    static class Patch_Game_InitNewGame
+    {
+        static void Postfix()
+        {
+            WorldComp.IsNewGame = true;
+        }
+    }
+
+    /*[HarmonyPatch(typeof(Game), "LoadGame")]
+    static class Patch_Game_LoadGame
+    {
+        static void Postfix()
+        {
+            ResearchTimeUtil.CreateBaseResearchDefs();
+            ResearchTimeUtil.ApplyFactor();
+        }
+    }*/
+}
---
+++ Source/Settings.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace ModifyResearchTime
+{
+    class Settings : Mod
+    {
+        public const string FLOAT_FORMAT = "####.####";
+        private static string settingsFactor = "1.00";
+
+        public static float SettingsFactor
+        {
+            get
+            {
+                if (ValidateInput())
+                {
+                    return float.Parse(settingsFactor);
+                }
+                return 1f;
+            }
+        }
+
+        public Settings(ModContentPack content) : base(content)
+        {
+            
+        }
+
+        public override string SettingsCategory()
+        {
+            return "ModifyResearchTime".Translate();
+        }
+
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+            Scribe_Values.Look<string>(ref settingsFactor, "ModifyResearchTime.Factor", "1.00", false);
+        }
+
+        public override void DoSettingsWindowContents(Rect rect)
+        {
+            Rect r = new Rect(0, 60, 600, 85);
+            GUI.BeginGroup(r);
+            r = new Rect(0, 0, 300, 20);
+            GUI.Label(r, "ModifyResearchTime.Factor".Translate() + ":");
+            r = new Rect(320, 0, 280, 20);
+            settingsFactor = GUI.TextField(r, settingsFactor);
+            if (Current.Game != null)
+            {
+                r = new Rect(320, 25, 200, 25);
+                if (GUI.Button(r, new GUIContent("ModifyResearchTime.Apply".Translate())))
+                {
+                    if (ValidateInput())
+                    {
+                        WorldComp.UpdateFactor(SettingsFactor);
+                        Messages.Message("ModifyResearchTime.ResearchTimesUpdated".Translate(), MessageSound.Benefit);
+                    }
+                }
+
+            }
+            GUI.EndGroup();
+        }
+
+        private static bool ValidateInput()
+        {
+            float f;
+            if (float.TryParse(settingsFactor, out f))
+            {
+                if (f <= 0)
+                {
+                    Messages.Message("Research Time Factor cannot be less than or equal to 0.", MessageSound.RejectInput);
+                    return false;
+                }
+            }
+            else
+            {
+                Messages.Message("Unable to parse Research Time Factor to a number.", MessageSound.RejectInput);
+                return false;
+            }
+            return true;
+        }
+    }
+}
